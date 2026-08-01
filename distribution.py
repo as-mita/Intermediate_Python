6a0e5b42f905f07f113d6834679dd6843271f3f7
as-mita <asmitathapaa5@gmail.com>
2026-08-01 21:48:10 +0545

    add Normal (Gaussian) Distribution

--- distribution.py
+++ distribution.py
@@ -4,3 +4,10 @@ import numpy as np
 uniform_data = np.random.uniform(0, 1, 10)
 
 print(uniform_data)
+
+import numpy as np
+
+# mean = 0, standard deviation = 1
+normal_data = np.random.normal(loc=0, scale=1, size=10)
+
+print(normal_data)
