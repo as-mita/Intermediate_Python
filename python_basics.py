023560bacac153b1865215f72c1c3e7c8bf09382
as-mita <asmitathapaa5@gmail.com>
2026-08-15 23:59:25 +0545

    add
    Python Basic revision

---
+++ python_basics.py
@@ -0,0 +1,20 @@
+"""
+Python Basic revision
+
+"""
+# 1. Variables and data Types
+
+name = "Asmita"
+age = 23
+height = 5.2
+is_learning = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("learning Python: ", is_learning)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_learning))
