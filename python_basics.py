1b8cfde7cacfdd41c0b639fbc3a7ca51623bda48
as-mita <asmitathapaa5@gmail.com>
2026-08-16 23:32:43 +0545

    revision

--- python_basics.py
+++ python_basics.py
@@ -18,3 +18,40 @@ print(type(name))
 print(type(age))
 print(type(height))
 print(type(is_learning))
+
+# ============================================================
+# 2. TYPE CONVERSION
+# ============================================================
+
+number_as_string = "100"
+
+number = int(number_as_string)
+decimal_number = float(number)
+
+print("\nType Conversion:")
+print(number)
+print(decimal_number)
+print(str(number))
+
+
+# ============================================================
+# 3. STRINGS
+# ============================================================
+
+message = "Python is powerful"
+
+print("\nStrings:")
+print(message)
+print(message.upper())
+print(message.lower())
+print(message.title())
+print(message.replace("powerful", "useful"))
+print(message[0])
+print(message[-1])
+print(message[0:6])
+
+# f-string
+language = "Python"
+level = "beginner"
+
+print(f"I am learning {language} at a {level} level.")
